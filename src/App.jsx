82ecb901d12524ed1ee205361d60107622d7c1ad
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-22 22:08:37 +0200

    fix tailwindsetup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import './App.css';
 
 const App = () => {
 	return (
 		<div>
-			<section className='h-screen bg-green-300 flex items-center justify-center text-center text-3xl'>
+			<section className='h-screen bg-green-300'>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
 					iste, ducimus quos adipisci libero saepe commodi facilis officia
