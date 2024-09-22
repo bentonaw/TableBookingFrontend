6f01dd51fe14cd1dcedd0e0a877f6553a25485dc
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-22 23:33:46 +0200

    done tailwindtest

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import './App.css';
 const App = () => {
 	return (
 		<div>
-			<section className='h-screen bg-green-300'>
+			{/* <section className='h-screen bg-green-300'>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
 					iste, ducimus quos adipisci libero saepe commodi facilis officia
 					accusamus! Quaerat veritatis itaque earum a quae vitae aspernatur
 					asperiores, placeat provident.
 				</p>
-			</section>
+			</section> */}
+			<div className='card'>This is a card.</div>
 		</div>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors:{
+        "greygrey" : "#f1f1f1",
+      }
+    },
 	},
 	plugins: [],
 };
