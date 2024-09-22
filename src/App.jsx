113a3234bac0e9b2fbfc760eaf91c42c3db02166
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-22 21:39:38 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div>
+			<section className='h-screen bg-green-300 flex items-center justify-center text-center text-3xl'>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+					iste, ducimus quos adipisci libero saepe commodi facilis officia
+					accusamus! Quaerat veritatis itaque earum a quae vitae aspernatur
+					asperiores, placeat provident.
+				</p>
+			</section>
+		</div>
+	);
+};
+
+export default App;
