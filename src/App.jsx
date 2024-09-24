e45650a6add00148bbb3a362d05513b48de49b3a
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-25 00:56:01 +0200

    fix header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout/Layout"
 import MainPage from "./pages/MainPage"
@@ -16,33 +15,6 @@ const App = () => {
 				</Route>
 			</Routes>
 		</Router>
-=======
-import './App.css';
-
-const App = () => {
-	return (
-		<div>
-			{/* <section className='h-screen bg-green-300'>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-					iste, ducimus quos adipisci libero saepe commodi facilis officia
-					accusamus! Quaerat veritatis itaque earum a quae vitae aspernatur
-					asperiores, placeat provident.
-				</p>
-			</section> */}
-			<div className='card'>This is a card.</div>
-		</div>
->>>>>>> ad4bbb234cb516d1a368111297851d5c422f172d
 	);
 };
-// <div className='bg-img' >
-		// 	<section className='h-screen bg-green-300 flex items-center justify-center text-center text-3xl'>
-		// 		<p>
-		// 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-		// 			iste, ducimus quos adipisci libero saepe commodi facilis officia
-		// 			accusamus! Quaerat veritatis itaque earum a quae vitae aspernatur
-		// 			asperiores, placeat provident.
-		// 		</p>
-		// 	</section>
-		// </div>
 export default App;
