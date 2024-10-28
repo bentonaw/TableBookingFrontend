0b797f0195017f7cef400fb9196a2e2933b1302c
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-29 00:53:22 +0100

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
 
 	return (
 		<header className='border-b'>
 			<div className='container px-4 h-12 flex items-center justify-between'>
-				<div className='flex items-center'>
-					Snacks
-				</div>
+				<div className='flex items-center'>Snacks</div>
+				<Button variant='outline'>
+					<Link to='/'>home</Link>
+				</Button>
 
 				<div className='ml-auto'>
-					<Button
-						variant='outline'>
-						Login
+					<Button variant='outline'>Login</Button>
+				</div>
+				<div className='ml-auto'>
+					<Button variant='outline'>
+						<Link to='/reservation'>Reservation</Link>
 					</Button>
 				</div>
 			</div>
