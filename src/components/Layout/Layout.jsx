45f517f91a1013535c6e49dd65aa211ab928a488
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-04 02:47:34 +0100

    fix layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer';
-import Header from '../Header';
+// import Header from '../Header';
 
 const Layout = () => {
+	
 	return (
 		<div className='flex flex-col h-screen overflow-hidden'>
-			<Header />
+			{/* <Header /> */}
 			<main className='flex-grow overflow-auto'>
 				<Outlet />
 			</main>
